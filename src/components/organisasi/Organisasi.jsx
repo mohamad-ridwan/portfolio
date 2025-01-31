377b9505154c84c58606977889244503ba40953f
mohamad-ridwan <mr643062@gmail.com>
2025-01-31 13:37:51 +0700

    update organisasi

--- src/components/organisasi/Organisasi.jsx
+++ src/components/organisasi/Organisasi.jsx
@@ -11,8 +11,60 @@ export default function Organisasi() {
         {
             img: 'gangco.jpeg',
             url: 'https://github.com/gangco-io'
+        },
+        {
+            img: 'komers.jpeg',
+            url: 'https://github.com/REALM-CHAT'
         }
     ])
+    const dataPengalaman = [
+        {
+            img: 'udex.jpeg',
+            url: 'https://udexmedia.com/',
+            pt: 'UDEX MEDIA',
+            deskripsi: <>
+                <p style={{
+                    textAlign: 'start',
+                    opacity: 0.7,
+                    fontSize: '14px'
+                }}>
+                    Perusahaan IT Consultant yang berada di Malaysia.
+                </p>
+            </>,
+            date: '2024 -',
+            dateIcon: <i class="fa-solid fa-calendar-days" style={{ fontSize: '14px', opacity: 0.7 }}></i>
+        },
+        {
+            img: 'gangco.jpeg',
+            url: 'https://github.com/gangco-io',
+            pt: 'Gangco',
+            deskripsi: <p style={{
+                textAlign: 'start',
+                opacity: 0.7,
+                fontSize: '14px'
+            }}>
+                Organisasi yang dibuat oleh Perusahaan UDEX MEDIA. Organisasi ini berkerjasama dengan agency Perusahaan <strong><a href="https://www.bizapp.com.my" target='_blank' style={{
+                    color: '#fff'
+                }}>bizapp</a></strong>.
+            </p>,
+            dateIcon: <i class="fa-solid fa-calendar-days" style={{ fontSize: '14px', opacity: 0.7 }}></i>,
+            date: '2024 -'
+        },
+        {
+            img: 'komers.jpeg',
+            url: 'https://github.com/REALM-CHAT',
+            pt: 'Komers',
+            dateIcon: <i class="fa-solid fa-calendar-days" style={{ fontSize: '14px', opacity: 0.7 }}></i>,
+            deskripsi: <p style={{
+                textAlign: 'start',
+                opacity: 0.7,
+                fontSize: '14px'
+            }}>
+                Merupakan Start Up yang dibangun oleh Perusahaan UDEX MEDIA.
+            </p>,
+            date: '2024 -'
+        }
+    ]
 
     return (
         <>
@@ -43,6 +95,57 @@ export default function Organisasi() {
                             )
                         })}
                     </ul>
+                    <div style={{
+                        marginTop: '5rem',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '70%',
+                    }}>
+                        <h3 style={{
+                            textAlign: 'start',
+                            color: '#fff'
+                        }}>Catatan Pengalaman</h3>
+                        <div style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            color: '#fff',
+                            gap: '1.5rem',
+                            marginTop: '1rem'
+                        }}>
+                            {dataPengalaman.map((e, i) => (
+                                <div key={i} style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    gap: '5px'
+                                }}>
+                                    <div style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        gap: '5px',
+                                        color: '#fff'
+                                    }}>
+                                        <Icons
+                                            img={e.img}
+                                            url={e.url}
+                                            height='30px'
+                                            width='30px'
+                                            borderRadius='50%'
+                                        />
+                                        <h3>{e.pt}</h3>
+                                    </div>
+                                    {e.deskripsi}
+                                    <div style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        gap: '5px'
+                                    }}>
+                                        {e.dateIcon}
+                                        <span style={{ opacity: 0.7, fontSize: '14px' }}>{e.date}</span>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
